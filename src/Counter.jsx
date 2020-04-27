c1c9c344a81c34d8d96c8c32b1d987890fced766
angelique-m08 <mora.angelique@gmail.com>
2020-04-27 15:11:20 +0200

    refactor(counter): add an input to define first value

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -7,6 +7,11 @@ const Counter = () => {
       <p>Le compteur est à : {count}</p>
       <button onClick={() => (setCount(count + 1))} >+1</button>
       <button onClick={() => (setCount(count - 1))} >-1</button>
+      <div>
+        <label for="count">Valeur de départ du compteur :</label>
+        <input type="number" id="counter" name="counter" onChange={event => setCount(Number(event.target.value))} />
+      </div>
+
     </div>
   )
 }
